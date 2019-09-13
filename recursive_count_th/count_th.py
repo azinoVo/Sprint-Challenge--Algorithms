ab82bdebf3e3872a1896076dc088c917a6dd0300
azinoVo <azinovo22@gmail.com>
2019-09-13 12:01:58 -0500

    implem count_th and works but test fails

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,42 @@
 '''
-Your function should take in a signle parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should take in a single parameter (a string `word`)
+Your function should return a count of how many occurrences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
+
+def count_th(word, count = 0):
+
+    # Counter to accumulate the number of occurrences as they happen
+    occurrences = count
+
+    # First two letters that we are checking
+    piece = word[0:2]
+    print("word", word)
+    print("pieces", piece)
+    print("occurrences", occurrences)
     
-    # TBC
-    
-    pass
+    # Base Case and returns the number of occurrences
+    # End at conclusion of word
+    if len(word) == 2:
+        if word == "th":
+            occurrences = count + 1
+            print("FINAL", occurrences)
+            return occurrences
+        else:
+            print("FINAL", occurrences)
+            return occurrences
+
+    # Function Calling itself slowly reaching the base case
+    elif len(word) > 2:
+        if piece == "th":
+            # Check if letters are "th", increment, and repeat the function
+            occurrences = count + 1
+            print("***", occurrences)
+            # Step by one letter at a time, cutting off the letter behind it each time
+            count_th(word[1:], occurrences)
+        else:
+            count_th(word[1:], occurrences)
+    else:
+            pass
+
+count_th("abcthxyz", 0)
