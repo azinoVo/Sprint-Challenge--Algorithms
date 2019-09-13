d3c35946ebd7334b19a20dcf07128a2799b0cca8
azinoVo <azinovo22@gmail.com>
2019-09-13 12:09:03 -0500

    added some fix to count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -11,9 +11,9 @@ def count_th(word, count = 0):
 
     # First two letters that we are checking
     piece = word[0:2]
-    print("word", word)
-    print("pieces", piece)
-    print("occurrences", occurrences)
+    # print("word", word)
+    # print("pieces", piece)
+    # print("occurrences", occurrences)
     
     # Base Case and returns the number of occurrences
     # End at conclusion of word
@@ -31,12 +31,15 @@ def count_th(word, count = 0):
         if piece == "th":
             # Check if letters are "th", increment, and repeat the function
             occurrences = count + 1
-            print("***", occurrences)
+            print("*** th Found***", occurrences)
             # Step by one letter at a time, cutting off the letter behind it each time
             count_th(word[1:], occurrences)
         else:
             count_th(word[1:], occurrences)
     else:
-            pass
+            print("FINAL", occurrences)
+            return occurrences
 
-count_th("abcthxyz", 0)
+count_th("") #0
+count_th("abcthxyz") #1
+count_th("abcthefthghith") #3
